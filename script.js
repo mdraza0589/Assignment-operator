7c4a8378e12ee3c7460de16723e28819d733d235
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-02-17 21:15:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var a = 5;
+var b = 7;
+var c= a + b;
+document.write("sum is = ",c);
+document.write("<br>");
+
+// var x=10.3;
+// var y=20;
+// var sub=y-x;
+// document.write(sub);
+// document.write("<br>");
+// document.write(typeof sub);
+// document.write("<br>");
+
+// var n=10.5;
+// var m=20;
+// var mul=m*n;
+// document.write("multiply of ",n," and ",m," is = ",mul);
+// document.write("<br>");
+// document.write(typeof mul);
+// document.write("<br>");
+
+// m+=6; 
+// document.write(m);
+
+// document.write("<br>");
+// var a=14;
+// document.write(a);
+
